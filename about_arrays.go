041e378c7b9624d27b1188368e286933730c0e21
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-04-27 16:26:29 +0200

    feat: solve arrays

--- about_arrays.go
+++ about_arrays.go
@@ -5,32 +5,32 @@ import "fmt"
 func aboutArrays() {
 	fruits := [4]string{"apple", "orange", "mango"}
 
-	Assert(fruits[0] == __string__) // indexes begin at 0
-	Assert(fruits[1] == __string__) // one is indeed the loneliest number
-	Assert(fruits[2] == __string__) // it takes two to ...tango?
-	Assert(fruits[3] == __string__) // there is no spoon, only an empty value
+	Assert(fruits[0] == "apple")  // indexes begin at 0
+	Assert(fruits[1] == "orange") // one is indeed the loneliest number
+	Assert(fruits[2] == "mango")  // it takes two to ...tango?
+	Assert(fruits[3] == "")       // there is no spoon, only an empty value
 
-	Assert(len(fruits) == __int__) // the length is what the length is
-	Assert(cap(fruits) == __int__) // it can hold no more
+	Assert(len(fruits) == 4) // the length is what the length is
+	Assert(cap(fruits) == 4) // it can hold no more
 
-	Assert(fruits == [4]string{}) // comparing arrays is not like comparing apples and oranges
+	Assert(fruits == [4]string{"apple", "orange", "mango"}) // comparing arrays is not like comparing apples and oranges
 
 	tasty_fruits := fruits[1:3]                           // defining oneself as a variation of another
-	Assert(fmt.Sprintf("%T", tasty_fruits) == __string__) //and get not a simple array as a result
-	Assert(tasty_fruits[0] == __string__)                 // slices of arrays share some data
-	Assert(tasty_fruits[1] == __string__)                 // albeit slightly askewed
+	Assert(fmt.Sprintf("%T", tasty_fruits) == "[]string") //and get not a simple array as a result
+	Assert(tasty_fruits[0] == "orange")                   // slices of arrays share some data
+	Assert(tasty_fruits[1] == "mango")                    // albeit slightly askewed
 
-	Assert(len(tasty_fruits) == __int__) // its length is manifest
-	Assert(cap(tasty_fruits) == __int__) // but its capacity is surprising!
+	Assert(len(tasty_fruits) == 2) // its length is manifest
+	Assert(cap(tasty_fruits) == 3) // but its capacity is surprising!
 
 	tasty_fruits[0] = "lemon" // are their shared roots truly identical?
 
-	Assert(fruits[0] == __string__) // has this element remained the same?
-	Assert(fruits[1] == __string__) // how about the second?
-	Assert(fruits[2] == __string__) // surely one of these must have changed
-	Assert(fruits[3] == __string__) // but who can know these things
+	Assert(fruits[0] == "apple") // has this element remained the same?
+	Assert(fruits[1] == "lemon") // how about the second?
+	Assert(fruits[2] == "mango") // surely one of these must have changed
+	Assert(fruits[3] == "")      // but who can know these things
 
 	veggies := [...]string{"carrot", "pea"}
 
-	Assert(len(veggies) == __int__) // array literals need not repeat an obvious length
+	Assert(len(veggies) == 2) // array literals need not repeat an obvious length
 }
